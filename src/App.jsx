fdfb041b2925b034ace4734c869d28ec11f6c813
Trithereon <eliottrassa@gmail.com>
2026-03-26 20:20:08 -0400

    Add vite and react template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <span>Content goes here.</span>
+    </>
+  );
+}
+
+export default App;
